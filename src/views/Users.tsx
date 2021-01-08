9ffd8d9566c61acab309f2b7c31ae31933089173
sunyang9876 <sunyang9876@hotmail.com>
2021-01-08 19:37:29 +1000

    add usertable

--- src/views/Users.tsx
+++ src/views/Users.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+// import Button from '@material-ui/core/Button';
+import UserTable from '../components/UserTable';
 
 const Users:React.FunctionComponent<{}> = () => {
+
+
+
   return (
     <div>
-      users
+      <UserTable />
     </div>
   )
 }

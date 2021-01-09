bb5e87a8cc663a89fc26d168ddf6a0daf3970d54
sunyang9876 <sunyang9876@hotmail.com>
2021-01-09 17:08:05 +1000

    create posts path and component

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import Users from '../views/Users'
+import Users from '../views/Users';
+import Posts from '../views/Posts';
 
 const Routes: React.FunctionComponent<{}> = () => {
   return (
     <Switch>
       <Route exact path="/users" component={Users} />
       <Route exact path="/"><Redirect to="/users" /></Route>
+      <Route exact path="/users/:id/posts" component={Posts} />
     </Switch>
   )
 }
---
+++ src/views/Posts.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Posts = () => {
+  return (
+    <div>
+      <p>posts</p>
+    </div>
+  )
+}
+
+export default Posts

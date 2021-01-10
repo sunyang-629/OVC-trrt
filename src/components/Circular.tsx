3cefd9a7a2ee9d23fc7c4c339c6a28fadd56d356
sunyang9876 <sunyang9876@hotmail.com>
2021-01-10 15:55:26 +1000

    complete utils test

--- src/components/Circular.tsx
+++ src/components/Circular.tsx
@@ -19,7 +19,6 @@ export default function CircularIndeterminate() {
 
   return (
     <div className={classes.root}>
-      {/* <CircularProgress /> */}
       <CircularProgress size={15} color="primary" />
     </div>
   );
---
+++ src/utils/filter.test.jsx
@@ -0,0 +1,16 @@
+// const filterByName = require('./filter')
+import {filterByName} from './filter'
+
+const array = [
+  { name: "Harry Potter" },
+  { name: "Lily Potter" },
+  { name: "RonTer Weasley" },
+  { name: "Hermione Granger" },
+  { name: "Draco Malfoy" },
+]
+const string = 'ter'
+
+test('should return name including ter', () => {
+  expect(filterByName(array, string))
+    .toEqual([{ name: "Harry Potter" }, { name: "Lily Potter" }, { name: "RonTer Weasley" }])
+})
